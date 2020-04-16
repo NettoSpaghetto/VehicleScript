adb6d2ef16588e29e563c768365ebe601bee0dd9
BlayneWright <41925552+NettoSpaghetto@users.noreply.github.com>
2020-04-16 01:55:13 +0100

    Testing Sound

--- lua/autorun/server/vehiclefunctions.lua
+++ lua/autorun/server/vehiclefunctions.lua
@@ -1,11 +1,18 @@
-hook.Add("CanExitVehicle", "AreSeatbeltsOn", function(veh, ply)
-    print("canexit run")
-    print(ply:GetNWBool("Seatbelt"))
-    if ply:GetNWBool("Seatbelt") == true then
-        DarkRP.notify(ply, 1, 6, "Take your seatbelt off first!")
-        return false
-    else
+hook.Add("PlayerButtonDown", "SeatbeltsOnEverybody", function(ply,key)
+
+    if ply:InVehicle() and key == KEY_J then
+        sound.Play( "sound/seatbelt/bucklein.wav", Vector(0,0,0), 75, 150, 0.5 )
+        ply:SetNWBool("Seatbelt", !ply:GetNWBool("Seatbelt", false))
+    end
+
+end)
+
+hook.Add("CanExitVehicle", "CheckExitVehicle", function(veh, ply)
+
+    if ply:GetNWBool("Seatbelt") == false then
         return true
+    elseif ply:GetNWBool("Seatbelt") == true then
+        return false
     end
 
 end)
