95e5e7be7bfdc67a0f5c2ef42a90cada75a7ce25
BlayneWright <41925552+NettoSpaghetto@users.noreply.github.com>
2020-04-16 20:10:39 +0100

    Workshop Ready.

--- lua/autorun/server/vehiclefunctions.lua
+++ lua/autorun/server/vehiclefunctions.lua
@@ -1,18 +1,18 @@
 hook.Add("PlayerButtonDown", "SeatbeltsOnEverybody", function(ply,key)
 
-    if ply:InVehicle() and key == KEY_J then
-        if ply:GetNWBool("Seatbelt") == false then
+    if ply:IsValid() and ply:InVehicle() and key == KEY_J then
+        if ply:GetNWBool("Seatbelt", false) == false then
             sound.Play( "vehiclesounds/bucklein.wav", ply:GetPos(), 75, 150, 0.5 )
-        elseif ply:GetNWBool("Seatbelt") == true then
+        elseif ply:GetNWBool("Seatbelt", true) == true then
             sound.Play( "vehiclesounds/buckleout.wav", ply:GetPos(), 75, 160, 0.5)
         end
         ply:SetNWBool("Seatbelt", !ply:GetNWBool("Seatbelt", false))
     end
 
     if ply:InVehicle() and key == KEY_H then
-        if ply:GetNWBool("EngineStatus") == false then
+        if ply:GetNWBool("EngineStatus", false) == false then
             sound.Play( "vehiclesounds/enginestart.wav", ply:GetPos(), 75, 90, 1 )
-        elseif ply:GetNWBool("EngineStatus") == true then
+        elseif ply:GetNWBool("EngineStatus", true) == true then
             sound.Play( "vehiclesounds/engineoff.wav", ply:GetPos(), 75, 100, 1)
         end
         ply:SetNWBool("EngineStatus", !ply:GetNWBool("EngineStatus", false))
